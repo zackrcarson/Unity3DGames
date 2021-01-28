b5d00b76d0d1f5b28f60331e18773eea31f95b2a
zackrcarson <zackrcarson@gmail.com>
2021-01-27 20:20:40 -0700

    Friendly AI character popup with game hints.
    Friendly AI character popup with game hints.

---
+++ TheDoomedPlanet/Assets/Scripts/AISpeechHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpeechHandler : MonoBehaviour
+{
+    // Config Parameters
+    [SerializeField] string[] dialogueInputs = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
